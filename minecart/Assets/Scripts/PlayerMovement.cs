59648871a1342f75496de453fe0aaa9d7b6b80c3
adketuri <adketuri@gmail.com>
2019-03-23 18:07:45 -0700

    Fix some animation issues, add a bit more level to play with

--- minecart/Assets/Scripts/PlayerMovement.cs
+++ minecart/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController2D controller;
     float horizontalMove = 0f;
-    float runSpeed = 40f;
+    float runSpeed = 60f;
     bool jump = false;
     bool facingRight = true;
 
@@ -43,6 +43,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Math.Abs(horizontalMove));
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
@@ -62,6 +63,8 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("IsFalling", false);
             fallen = false;
             gameObject.transform.position = initialPos;
+            Rigidbody2D rb2d = gameObject.GetComponent<Rigidbody2D>();
+            rb2d.velocity = Vector2.zero;
         }
     }
 
@@ -73,8 +76,6 @@ public class PlayerMovement : MonoBehaviour
     // return a degree 0 to 90 for slope
     private float getGroundAngle()
     {
-        float groundAngle = -1;
-
         // define a ray starting at this object's position
         // in this object's local downward direction
         Ray2D ray = new Ray2D(transform.position, -transform.up);
@@ -82,36 +83,14 @@ public class PlayerMovement : MonoBehaviour
         // shoot that ray and get the results
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, groundCheckDistance, groundLayers);
         // something was hit
+        float angle = 0;
         if (hit.collider != null)
         {
-
-            // get which direction the ground is facing
-            Vector3 groundFacingDirection = hit.normal;
-
-            // y axis (when ground is perfectly flat, the normal equals this)
-            Vector3 verticalDirection = Vector3.up; // same as Vector3(0,1,0)
-
-            // find the angle between the two directions
-            // returns the acute angle (0 to 180)
-            groundAngle = Vector3.SignedAngle(verticalDirection, groundFacingDirection, verticalDirection);
-
-            // if the angle is greater than 90, make it go back towards 0 instead of up to 180
-            if (groundAngle > 90)
-            {
-                //groundAngle = 90 - (groundAngle - 90);
-            }
-
-            // Let's flip the angle when we face LEFT
-            if (!facingRight)
-            {
-                groundAngle *= -1;
-            }
-
-            //Debug.Log(facingRight + " " + groundAngle);
+            // get the angle from the normal (perpendicular to slope) and the up direction
+            angle = Vector2.SignedAngle(hit.normal, Vector2.up) * (facingRight ? -1 : 1);
             
         }
-
-        return groundAngle;
+        return angle;
     }
 
     private void FixedUpdate()
