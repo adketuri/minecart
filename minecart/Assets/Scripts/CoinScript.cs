b7f26cedfedb7adbc393c2080fa0f0f468ea56e5
adketuri <adketuri@gmail.com>
2019-03-24 22:38:21 -0700

    Add collectible coins

---
+++ minecart/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ENTER");
+        if (other.name == "Character")
+        {
+            SoundManagerScript.PlaySound(SoundManagerScript.COIN);
+            Destroy(gameObject);
+        }
+    }
+}
